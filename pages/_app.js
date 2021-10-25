26a7dcc732863fbc336241656b24b314a011255c
fatan <fatan.aminullah.j@gmail.com>
2021-10-25 15:09:10 +0700

    config gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import "assets/scss/main.scss"
 
 import { LoadingProvider } from "context/loading-context"
+import { useEffect } from "react"
+import TagManager from "react-gtm-module"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: "GTM-P9KXR8G",
+    })
+  }, [])
   return (
     <LoadingProvider>
       <Component {...pageProps} />
