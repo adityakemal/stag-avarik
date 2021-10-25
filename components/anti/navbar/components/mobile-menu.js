565c8e9fa437fa448d467582796501891859cf1d
laskar-ksatria <dock_fest@yahoo.co.id>
2021-10-25 13:56:10 +0700

    fix weapons navigation

--- components/anti/navbar/components/mobile-menu.js
+++ components/anti/navbar/components/mobile-menu.js
@@ -1,20 +1,27 @@
 import React from "react"
 import { Link } from "components/anti/link/link"
 import { Accordion } from "components/anti/accordion/accordion"
-
+import { useRouter } from 'next/router'
 import { scroller, Element } from "react-scroll"
 
 const MobileMenu = ({ navExpand, variant, handleMenuMobile }) => {
-  const handleClick = (content) => {
-    scroller.scrollTo(content, {
-      duration: 500,
-      delay: 100,
-      smooth: true,
-    })
+
+  const Router = useRouter()
+
+  const handleClick = (content, url) => {
+    if (url) {
+      Router.push(url)
+    } else {
+      scroller.scrollTo(content, {
+        duration: 500,
+        delay: 100,
+        smooth: true,
+      })
+    }
   }
 
-  const handleCloseMenu = (content) => {
-    handleMenuMobile(false, () => handleClick(content))
+  const handleCloseMenu = (content, url) => {
+    handleMenuMobile(false, () => handleClick(content, url))
   }
   return (
     <>
@@ -70,20 +77,24 @@ const MobileMenu = ({ navExpand, variant, handleMenuMobile }) => {
                   Team
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item"
+                onClick={() => handleCloseMenu("", "/litepaper")}
+              >
                 <Link
                   className="nav-link anim-6"
                   activeClassName="active"
-                  to="/litepaper"
+                // to="/litepaper"
                 >
                   Lite Paper
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item"
+                onClick={() => handleCloseMenu("", "/weapons")}
+              >
                 <Link
                   className="nav-link anim-7"
                   activeClassName="active"
-                  to="/weapons"
+                // to="/weapons"
                 >
                   Weapons
                 </Link>
