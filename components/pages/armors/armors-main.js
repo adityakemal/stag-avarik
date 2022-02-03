2e151a9d9994fa1936e6a5b106cb890ead12fc84
fatan <fatan.aminullah.j@gmail.com>
2022-02-04 03:18:52 +0700

    revamp armors page

--- components/pages/armors/armors-main.js
+++ components/pages/armors/armors-main.js
@@ -1,76 +1,106 @@
+import { useScrollAnim } from "components/hooks/hooks"
+import book from "assets/img/common/book.png"
+import left from "assets/img/common/ornament-left.png"
+import right from "assets/img/common/ornament-right.png"
+import logo from "assets/img/common/logo_main-icon.png"
+import logoLight from "assets/img/knights/icon_light.png"
 import knightIcon from "assets/img/common/icon_knight.png"
 import marksmanIcon from "assets/img/common/icon_marksmen.png"
 import wizardIcon from "assets/img/common/icon_wizard.png"
-import knight from "assets/img/knights/armor_legendary.jpg"
-import marksman from "assets/img/marksmen/armor_legendary.jpg"
-import wizard from "assets/img/wizards/armor_legendary.jpg"
-import AvarikButton from "components/avarik-saga/avarik-button"
-import AvarikCards from "components/avarik-saga/avarik-cards"
-import { useScrollAnim } from "components/hooks/hooks"
-import { useState } from "react"
+import knight from "assets/img/armors/armors-knight.png"
+import arrowRight from "assets/img/common/arrow-right.png"
+import { Slider } from "components/anti"
 
-const indicator = [
+const data = [
   {
     name: "Knight",
-    image: knightIcon,
-    char: knight,
-    description: "Armors hold rarities similar to the model for Avarik Heroes themselves. There are 4 rarities for armors. The rarity influences 2 key principles in the game: Greater base stats on the armor. Enhances the skill power of both active and passive skills of the Hero."
+    icon: knightIcon,
+    image: knight,
+    description: "A symbol of defense, honor, and courage, the Knight’s Armor protects the mighty warrior as they fight ferociously."
   },
   {
     name: "Wizard",
-    image: wizardIcon,
-    char: wizard,
-    description: "Armors hold rarities similar to the model for Avarik Heroes themselves. There are 4 rarities for armors. The rarity influences 2 key principles in the game: Greater base stats on the armor. Enhances the skill power of both active and passive skills of the Hero."
+    icon: wizardIcon,
+    image: knight,
+    description: "Magical Robes and Cloaks with an affinity for considerable magical presence that will serve as magical protection."
   },
   {
     name: "Marksman",
-    image: marksmanIcon,
-    char: marksman,
-    description: "Armors hold rarities similar to the model for Avarik Heroes themselves. There are 4 rarities for armors. The rarity influences 2 key principles in the game: Greater base stats on the armor. Enhances the skill power of both active and passive skills of the Hero."
+    icon: marksmanIcon,
+    image: knight,
+    description: "Specially built to accommodate the nimble Marksmen and their sprightly attacks, these Armors must also defend them for their angry foes."
   },
 ]
 
+
 const ArmorsMain = () => {
   const [trigger, anim] = useScrollAnim()
-  const [selectedFaction, setSelectedFaction] = useState(indicator[0])
   return (
-    <section className={`sc-armors-main cover-full ${selectedFaction.name.toLowerCase()}`} ref={trigger}>
+    <section className="sc-armors-main" ref={trigger}>
       <div className="py-main h-100">
         <div className="container">
-          <div className="row justify-content-between">
-            <div className="col-md-4 col-text">
-              <h5 className={`label ${anim(1)}`}>Armors</h5>
-              <h1 className={`title ${anim(2)}`}>{selectedFaction.name}</h1>
-              <p className={`description ${anim(3)}`}>
-                {selectedFaction.description}
-              </p>
-              <AvarikButton
-                text="Watch Armors Trailer"
-                link="https://youtu.be/aGgHFL2Nbd4"
-                target="_blank"
-              />
-            </div>
-            <div className="col-md-8 col-img">
-              <img
-                src={selectedFaction.char}
-                className={`character ${anim(4, "revealInDown")}`}
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="row row-indicator">
-            {indicator.map((item) => (
-              <div className="col-4">
-                <AvarikCards
-                  label={item.name}
-                  image={item.image}
-                  className="mx-4"
-                  imgClassName="indicator-img"
-                  isActive={selectedFaction === item}
-                  onClick={() => setSelectedFaction(item)}
-                />
+          <div className="row">
+            <div className="col-md-5 col-text">
+              <div className="title-wrapper">
+                <img src={logo} className="logo" alt="" />
+                <img src={left} className="left" alt="" />
+                <h1 className={anim(1)}>
+                  Armors
+                </h1>
+                <img src={right} className="right" alt="" />
               </div>
-            ))}
+              <p className={anim(2)}>
+                Armors hold rarities similar to the model for Avarik Heroes themselves. There are 4 rarities for armors. The rarity influences 2 key principles in the game: Greater base stats on the armor. Enhances the skill power of both active and passive skills of the Hero.
+              </p>
+            </div>
+            <div className="col-md-7 col-img">
+              <img src={book} className="img-fluid img-book" alt="" />
+              <Slider
+                fade
+                infinite
+                arrowsMdDown={true}
+                arrowsSmDown={true}
+                showInitial={1}
+                showXxlDown={1}
+                showXlDown={1}
+                showLgDown={1}
+                showMdDown={1}
+                showSmDown={1}
+                dotsInitial={true}
+                dotsXxlDown={true}
+                dotsXlDown={true}
+                dotsLgDown={true}
+                dotsMdDown={true}
+                dotsSmDown={true}
+                className="book-slider"
+                nextArrow={<img src={arrowRight} />}
+                customPaging={(i) => (
+                  <button>
+                    <div className="overlay-hover" />
+                    <img src={data[i].icon} className={`dots-img ${anim(i + 2)}`} />
+                  </button>
+                )}
+              >
+                {data?.map((item, i) => (
+                  <div className="row book-wrapper" key={i}>
+                    <div className="col-6 col-img">
+                      <img src={item.image} className={`img-fluid ${anim(i + 2, "revealInDown")}`} alt="" />
+                    </div>
+                    <div className="col-6 col-text">
+                      <div className="title-wrapper">
+                        <img src={logoLight} className="logo" alt="" />
+                        <img src={left} className="left" alt="" />
+                        <h4 className={anim(i + 3)}>
+                          {item.name}
+                        </h4>
+                        <img src={right} className="right" alt="" />
+                      </div>
+                      <p className={anim(i + 4)}>{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+            </div>
           </div>
         </div>
       </div>
