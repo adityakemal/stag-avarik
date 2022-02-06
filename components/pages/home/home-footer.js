b899e95eba63789226692fd6df9c9f0c1e55c7d8
fatan <fatan.aminullah.j@gmail.com>
2022-02-07 01:58:21 +0700

    revamp home footer

--- components/pages/home/home-footer.js
+++ components/pages/home/home-footer.js
@@ -1,6 +1,6 @@
 import AvarikButton from "components/avarik-saga/avarik-button"
+import AvarikTitle from "components/avarik-saga/avarik-title"
 import { useScrollAnim } from "components/hooks/hooks"
-
 
 const HomeFooter = () => {
     const [trigger, anim] = useScrollAnim()
@@ -8,24 +8,27 @@ const HomeFooter = () => {
         <section className="sc-home-footer" ref={trigger}>
             <div className="py-main">
                 <div className="container d-flex align-items-center flex-column mx-auto mw-md">
-                    <h1 className={`title ${anim(1, "revealInUp")} mb-4`}>
-                        Step Forth and Craft Your Own Adventure
-                    </h1>
+                    <AvarikTitle title="Join the Community" titleClassName={`title ${anim(1, "revealInUp")} mb-4`} />
                     <p className={`description ${anim(2, "revealInUp")} mb-5`}>
-                        View the official <strong>Avarik Saga Universe </strong> on <strong> OpenSea </strong>
+                        Become a part of our community for exclusives updates!
                     </p>
-                    <div className="row w-100 justify-content-center">
+                    <div className="row w-100 justify-content-between">
                         <AvarikButton
                             link="https://opensea.io/collection/avariksagauniverse"
                             target="_blank"
-                            text="Main collection"
+                            text="Get Started"
                             className={`ml-md-0 ${anim(3)}`}
                         />
-                        {/* <AvarikButton
-                            link="https://game-avariksaga-stag.agatedev.net/"
+                        <AvarikButton
+                            link="/factions"
                             target="_blank"
-                            text="Try Game Demo"
-                        /> */}
+                            text="Factions"
+                        />
+                        <AvarikButton
+                            link="/class"
+                            target="_blank"
+                            text="Characters"
+                        />
                     </div>
                 </div>
             </div>
