fcfab07386da9dc91587f6dcb434eb8bb6d0e353
Kemal Aditya Zulfikar <kadityafikar@gmail.com>
2022-07-15 02:28:24 +0700

    test reinstall node modules

--- next.config.js
+++ next.config.js
@@ -1,13 +1,30 @@
 const withImages = require("next-images");
 const { publicRuntimeConfig, webpack } = withImages();
-const withSass = require('next-dart-sass')
+// const withSass = require('next-dart-sass')
 
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
-  },
+// module.exports = withSass({
+//   cssModules: true,
+//   cssLoaderOptions: {
+//     importLoaders: 1,
+//     localIdentName: "[local]___[hash:base64:5]",
+//   },
+//   reactStrictMode: true,
+//   publicRuntimeConfig,
+//   webpack,
+//   images: {
+//     // loader: 'akamai',
+//     // path: '/',
+//     disableStaticImages: true,
+//     domains: ["lh3.googleusercontent.com"],
+//     formats: ["image/avif", "image/webp"],
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   }
+// })
+
+
+module.exports = {
   reactStrictMode: true,
   publicRuntimeConfig,
   webpack,
@@ -21,4 +38,4 @@ module.exports = withSass({
   eslint: {
     ignoreDuringBuilds: true,
   }
-})
+}
