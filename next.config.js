cc1fb0c97d5c47c5c1a4583ff7f3491f8c47116f
Kemal Aditya Zulfikar <kadityafikar@gmail.com>
2022-07-15 02:58:39 +0700

    wth sass

--- next.config.js
+++ next.config.js
@@ -1,33 +1,14 @@
 const withImages = require("next-images");
 const { publicRuntimeConfig, webpack } = withImages();
-// const withSass = require('next-dart-sass')
-
-// module.exports = withSass({
-//   cssModules: true,
-//   cssLoaderOptions: {
-//     importLoaders: 1,
-//     localIdentName: "[local]___[hash:base64:5]",
-//   },
-//   reactStrictMode: true,
-//   publicRuntimeConfig,
-//   webpack,
-//   images: {
-//     // loader: 'akamai',
-//     // path: '/',
-//     disableStaticImages: true,
-//     domains: ["lh3.googleusercontent.com"],
-//     formats: ["image/avif", "image/webp"],
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   }
-// })
-
+const path = require('path')
 
 module.exports = {
   reactStrictMode: true,
   publicRuntimeConfig,
   webpack,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   images: {
     // loader: 'akamai',
     // path: '/',
