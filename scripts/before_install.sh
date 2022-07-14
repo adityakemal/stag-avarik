dc259b24e3f194f496696b314e24074a30d71bea
Clovis Warlop <clovis@warlop.be>
2022-07-14 20:20:47 +0700

    CI CD - Update 2

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,6 @@
 cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
 yum -y install nodejs npm
+echo "Completed NODEJS Installation"
+npm install --global yarn
+echo "Completed YARN Installation"
