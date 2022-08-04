299a074cc54dd53d120a9941d02c168ea3230c73
Kemal Aditya Zulfikar <kadityafikar@gmail.com>
2022-08-04 14:57:56 +0700

    add new json2

--- components/pages/gallery/modal.jsx
+++ components/pages/gallery/modal.jsx
@@ -136,7 +136,7 @@ export default function ModalPic({ open, handleClose, currentItem, isFactionPage
               />
               <RowModal
                 title={<div className="">Description</div>}
-                content={currentItem?.description.replaceAll('�',`'`)}
+                content={currentItem?.description.replaceAll('�', `'`)}
                 titleRow='col-5'
               />
               <RowModal
